90044f7dd96ddc09962e274cd963c3ed1ffa1f47
JonJimenez <jonathanjimenez0745@gmail.com>
2020-12-09 15:37:15 -0500

    update

--- src/main/java/login/App.java
+++ src/main/java/login/App.java
@@ -36,7 +36,6 @@ public class App
                     System.out.println("Changing views");
                     bHome.setVisible(false);
                     bHomeInv.setVisible(true);
-                    
                 }  
             }      
         );
